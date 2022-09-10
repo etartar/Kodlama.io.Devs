cf824f78d0738170487dcab9c9d73c3f65759821
Emir TARTAR <emirtartar35@gmail.com>
2022-09-10 16:47:17 +0300

    Delete Authorize attribute

--- src/project/WebAPI/Controllers/UsersController.cs
+++ src/project/WebAPI/Controllers/UsersController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : BaseController
